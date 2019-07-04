7606f343822d1e317544763d53df75a56668ab26
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-04 02:23:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
  //return object;
  //var newObj =  new Object(object,{ key: value });
  //return newObj;
- var newObj = Object.assign({}, obj)
+ var newObj = Object.assign({}, object, { key: value });
+ return newObj;
 }
