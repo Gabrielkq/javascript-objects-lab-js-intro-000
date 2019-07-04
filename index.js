f0c996a99fc9dbad8445d1693458aee5099365b7
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-04 02:10:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
  //object[key] = value;
  //return object;
- var newObj =  Object.assign({}; object)
+ var newObj =  Object.assign({}, object, {key: value});
+ return newObj;
 }
